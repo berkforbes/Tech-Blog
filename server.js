d70bccd01cfb8c404671726b0dbd2b45e07f0985
Berk Forbes <91864476+berkforbes@users.noreply.github.com>
2022-03-06 21:53:15 -0700

    user, post, comment routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const controller = require("./controllers");
 
 const app = express();
 const PORT = process.env.PORT || 3003;
@@ -27,10 +28,10 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+app.use("/", controller);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
