d88c2f073035b1c987dad670359ef64597d26834
Berk Forbes <91864476+berkforbes@users.noreply.github.com>
2022-03-08 00:17:25 -0700

    login and signup

---
+++ controllers/public/javascript/signup.js
@@ -0,0 +1,25 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-input-signup').value.trim();
+    const password = document.querySelector('#password-input-signup').value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username: username,
+          password: password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+
+  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
