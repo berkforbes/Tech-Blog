0df9e7e4e9aeca1791eb3435785b4d46fa27dc2a
Berk Forbes <91864476+berkforbes@users.noreply.github.com>
2022-04-10 21:45:37 -0600

    debug

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,8 @@
 const router = require("express").Router();
 const homeRoutes = require("./home-routes");
-const commentRoutes = require("./api/comment-routes");
-const userRoutes = require("./api/user-routes");
-const postRoutes = require("./api/post-routes");
+const apiRoutes = require("./api/")
 
 router.use("/", homeRoutes);
-router.use("/api/comments", commentRoutes);
-router.use("/api/users", userRoutes);
-router.use("/api/posts", postRoutes);
+router.use("/api", apiRoutes)
 
 module.exports = router;
