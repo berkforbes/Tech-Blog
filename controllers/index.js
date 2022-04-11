39bc04625daf43f13707ec6013f1fdf8afe7e208
Berk Forbes <91864476+berkforbes@users.noreply.github.com>
2022-04-10 22:20:30 -0600

    routes updated

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,10 @@
-const router = require("express").Router();
-const homeRoutes = require("./home-routes");
-const commentRoutes = require("./api/comment-routes");
-const userRoutes = require("./api/user-routes");
-const postRoutes = require("./api/post-routes");
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
-router.use("/", homeRoutes);
-router.use("/api/comments", commentRoutes);
-router.use("/api/users", userRoutes);
-router.use("/api/posts", postRoutes);
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
